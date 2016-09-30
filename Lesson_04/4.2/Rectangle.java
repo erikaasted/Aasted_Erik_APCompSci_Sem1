8f8d6a8b16851f8b6132038081aa161cae7f217f
tirefrier <erikaasted@yahoo.com>
2016-09-30 09:01:19 -0700

    finished rectange.java

---
+++ Lesson_04/4.2/Rectangle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Rectangle
+{
+	static double l, w;
+	static double perimeter;
+
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+
+		System.out.println("What is the length?");
+		l = kb.nextDouble();
+		kb.nextLine();
+		System.out.println("What is the width?");
+		w = kb.nextDouble();
+		kb.nextLine();
+
+		calcPerim();
+
+		print();
+	}
+
+	public static void calcPerim()
+	{
+		perimeter = 2 * (l + w);
+	}
+
+	public static void print()
+	{
+		System.out.println("The perimeter is " + perimeter);
+	}
+}
