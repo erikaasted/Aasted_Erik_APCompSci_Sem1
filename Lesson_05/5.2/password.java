b946bfae555996d8b6f15d15edcd4f2777f68900
tirefrier <erikaasted@yahoo.com>
2016-10-15 15:42:27 -0700

    finished lesson 5

--- Lesson_05/5.2/password.java
+++ Lesson_05/5.2/password.java
@@ -2,21 +2,48 @@ import java.util.Scanner;
 
 public class password
 {
+
 	public static void main(String[]args)
+	{
+		String correctusername = "tirefrier";
+		String correctpassword = "password";
+
+		boolean correct = passCheck(correctusername, correctpassword);
+
+		if (correct == true)
+			System.out.println("Access Granted");
+	}
+
+	public static boolean passCheck(String correctusername, String correctpassword)
 	{
 		Scanner kb = new Scanner(System.in);
 
-		String username, password;
+		System.out.println("Enter Username:");
+		String username = kb.nextLine();
+		System.out.println("Enter Password:");
+		String password = kb.nextLine();
 
-		username = "tirefrier";
-		password = "password";
+		if (username.equals(correctusername) && password.equals(correctpassword))
+		{
+			return true;
+		}
 
-
-	}
-
-	public static boolean passCheck(String username, String password);
-	{
-		if (username = 
+		if (!username.equals(correctusername))
+		{
+			if (password.equals(correctpassword))
+			{
+				System.out.println("Incorrect Username");
+				passCheck(correctusername, correctpassword);
+				return true;
+			}
+			if (!(password.equals(correctpassword)))
+			{
+				System.out.println("Incorrect Username and Password");
+				passCheck(correctusername, correctpassword);
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
